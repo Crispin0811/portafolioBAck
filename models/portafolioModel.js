03a5ca196f498ed52399e6559970691c673f7c51
kevin crispin <kevincrisgon@gmail.com>
2021-03-22 19:39:23 -0500

    BD acepta descripciones largas

--- models/portafolioModel.js
+++ models/portafolioModel.js
@@ -1,82 +1,3 @@
-// const mongoose = require("mongoose");
-// let Schema = mongoose.Schema;
-// const slug = require("slug");
-// const shortid = require("shortid");
-
-// const Portafolio = new Schema({
-//   titulo: {
-//     type: String,
-//     trim: true,
-//   },
-//   portadaImg: {
-//     type: String,
-//     trim: true,
-//     default: "https://res.cloudinary.com/duzf4c5dl/image/upload/v1615930802/Portafolio/imagenes/images_hkctgg.png",
-//   },
-//   descripcion: [
-//     {
-//       principal: {
-//         type: String,
-//         trim: true,
-//       },
-//       secundario: {
-//         type: String,
-//         trim: true,
-//       },
-//     },
-//   ],
-//   urlGit: {
-//     type: String,
-//     trim: true,
-//   },
-//   imgs: [
-//     {
-//       type: String,
-//       trim: true,
-//     },
-//   ],
-//   tipo: {
-//     type: String,
-//     trim: true,
-//   },
-//   tecnologias: [
-//     {
-//       back: [
-//         {
-//           type: Boolean,
-//           default: false,
-//         },
-//       ],
-//       frond: [
-//         {
-//           type: Boolean,
-//           default: false,
-//         },
-//       ],
-//       fullStack: [
-//         {
-//           type: Boolean,
-//           default: false,
-//         },
-//       ],
-//     },
-//   ],
-
-//   url: {
-//     type: String,
-//     trim: true,
-//   },
-// });
-
-// Portafolio.pre("save", async function (next) {
-//   const url = `${slug(this.titulo)}-${shortid.generate()}`;
-//   this.url = url;
-
-//   next();
-// });
-
-// module.exports = mongoose.model("Portafolios", Portafolio);
-
 const { Sequelize } = require("sequelize");
 
 const db = require("../config/db");
@@ -93,7 +14,7 @@ const Portafolios = db.define(
       type: Sequelize.STRING(100),
     },
     descripcion: {
-      type: Sequelize.STRING,
+      type: Sequelize.STRING(10000),
     },
     urlGit: {
       type: Sequelize.STRING(100),
