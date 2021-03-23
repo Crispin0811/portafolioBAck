d62b82ad10ced4f24fa2b94f926ff75932f83ff1
kevin crispin <kevincrisgon@gmail.com>
2021-03-23 18:10:39 -0500

    whiteList agregado

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors");
 require("dotenv").config({ path: "variables.env" });
-
 
 app.use(express.static("public"));
 
@@ -20,6 +20,21 @@ db.sync()
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
+
+let whiteList = [
+  "http://localhost:8001",
+  "https://crispin0811.github.io/kevinCG/",
+];
+let corsOpciones = {
+  origin: function (origin, cb) {
+    if (whiteList.indexOf(origin) !== -1) {
+      cb(null, true);
+    } else {
+      cb(new Error("Erro con el cors"));
+    }
+  },
+};
 
 // RUTAS
 app.use("/api", require("./routes/todosRoute"));
