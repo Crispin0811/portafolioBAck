13d2d1edb0bfefb705202b69a9b9e3df54e87e84
kevin crispin <kevincrisgon@gmail.com>
2021-03-22 21:36:21 -0500

    Error ENOENT solucionado

--- config/multer.js
+++ config/multer.js
@@ -2,14 +2,12 @@ const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    // cb(null, path.join(__dirname, "../public/img/") );
-    cb(null, "./public/img/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname)); //Appending extension
+  destination: path.join(__dirname, "public/img"),
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
   },
 });
+
 const upload = multer({
   storage: storage,
   fileFilter: function (req, file, cb) {
